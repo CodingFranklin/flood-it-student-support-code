682cc76b317c23f87e1bf76c7e6f519c018f8991
CodingFranklin <frankpu2004@gmail.com>
2025-01-24 11:32:14 -0500

    Floodit code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 FloodIt! Student Support Code
 
 Revision: Fall 2024
+
+[Question 1] What function roughly fits that graph? Hint: possibilities are 
+[Question 2] What is the time complexity of your flood() function based on analyzing its code? Answer the question in your write-up and write the complexity as a line of comment in your code above the function.
+[Question 3] Does your analysis match up with what you see in the graph?
+If not, double check your analysis.
+[Question 4] What data structures did you use (linked lists, arrays…) ? Is the time complexity of your flood function the best it can be or can you do better? Why?
--- src/Flood.java
+++ src/Flood.java
@@ -11,7 +11,22 @@ public class Flood {
                               LinkedList<Coord> flooded_list,
                               Tile[][] tiles,
                               Integer board_size) {
-        // YOUR CODE
+        for (int i = 0; i < flooded_list.size(); ++i) {
+            List<Coord> neighbors = new ArrayList<>();
+            neighbors = flooded_list.get(i).neighbors(board_size);
+            for (Coord neighbor : neighbors){
+                //skip if it was already checked
+                if (flooded_list.contains(neighbor)){
+                    continue;
+                }
+                else if (tiles[flooded_list.get(i).getY()][flooded_list.get(i).getX()].getColor()
+                        .equals(tiles[neighbor.getY()][neighbor.getX()].getColor())){
+                    flooded_list.add(neighbor);
+                }
+            }
+        }
+
+
     }
 
     // An alternative implementation goes here.
@@ -19,7 +34,29 @@ public class Flood {
                              LinkedList<Coord> flooded_list,
                              Tile[][] tiles,
                              Integer board_size) {
-        // YOUR CODE
+        Queue<Coord> copy_flooded_list = new LinkedList<>(flooded_list);
+        Set<Coord> floodedSet = new HashSet<>(flooded_list);
+
+        List<Coord> neighbors = new ArrayList<>();
+        if (copy_flooded_list != null) {
+            while (!copy_flooded_list.isEmpty()){
+                Coord first = copy_flooded_list.poll();
+                neighbors = first.neighbors(board_size);
+                for (Coord neighbor : neighbors){
+                    //skip if it was already checked (use hashset to check)
+                    if (floodedSet.contains(neighbor)){
+                        continue;
+                    }
+                    else if (tiles[first.getY()][first.getX()].getColor()
+                            .equals(tiles[neighbor.getY()][neighbor.getX()].getColor())){
+                        copy_flooded_list.add(neighbor);
+                        floodedSet.add(neighbor);
+                        flooded_list.add(neighbor);
+                    }
+                }
+            }
+
+        }
     }
 
 }
