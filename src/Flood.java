9227c1fc78ef55510e968d40334ac8991c5a71f0
CodingFranklin <frankpu2004@gmail.com>
2025-01-27 15:48:06 -0500

    Update Flood.java

--- src/Flood.java
+++ src/Flood.java
@@ -8,14 +8,17 @@ public class Flood {
 
     // Students implement this flood function.
     public static void flood(WaterColor color,
-                              LinkedList<Coord> flooded_list,
-                              Tile[][] tiles,
-                              Integer board_size) {
+                             LinkedList<Coord> flooded_list,
+                             Tile[][] tiles,
+                             Integer board_size) {
+        // Here for loop takes O(n)
         for (int i = 0; i < flooded_list.size(); ++i) {
             List<Coord> neighbors = new ArrayList<>();
+            // Here the get function of linked list also takes O(n)**
             neighbors = flooded_list.get(i).neighbors(board_size);
             for (Coord neighbor : neighbors){
                 //skip if it was already checked
+                // Here the contains function of linked list also takes O(n)**
                 if (flooded_list.contains(neighbor)){
                     continue;
                 }
@@ -25,8 +28,6 @@ public class Flood {
                 }
             }
         }
-
-
     }
 
     // An alternative implementation goes here.
@@ -34,16 +35,21 @@ public class Flood {
                              LinkedList<Coord> flooded_list,
                              Tile[][] tiles,
                              Integer board_size) {
+        // Here copy the list takes O(n)
         Queue<Coord> copy_flooded_list = new LinkedList<>(flooded_list);
+        // Here copy the list takes O(n)
         Set<Coord> floodedSet = new HashSet<>(flooded_list);
 
         List<Coord> neighbors = new ArrayList<>();
         if (copy_flooded_list != null) {
+            // Here while loop takes O(n)
             while (!copy_flooded_list.isEmpty()){
                 Coord first = copy_flooded_list.poll();
                 neighbors = first.neighbors(board_size);
+                // Here for loop takes maximum O(4)
                 for (Coord neighbor : neighbors){
                     //skip if it was already checked (use hashset to check)
+                    // Here contains function of HashSet takes O(1)
                     if (floodedSet.contains(neighbor)){
                         continue;
                     }
